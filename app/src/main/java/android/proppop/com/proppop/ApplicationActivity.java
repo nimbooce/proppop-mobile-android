ed8492a4864e595eb037905239ab65813d13fa5e
Nima A <nimaa@benetech.org>
2016-08-31 20:35:03 -0400

    Initial Layout for app

---
+++ app/src/main/java/android/proppop/com/proppop/ApplicationActivity.java
@@ -0,0 +1,10 @@
+package android.proppop.com.proppop;
+
+import android.app.Activity;
+
+/**
+ * Created by animal@martus.org on 7/15/16.
+ */
+
+public class ApplicationActivity extends Activity {
+}
